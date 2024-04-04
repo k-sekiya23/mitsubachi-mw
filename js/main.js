b321150f7a93db77e39bee853f2ea1708b0c3179
k-sekiya23 <158592486+k-sekiya23@users.noreply.github.com>
2024-04-05 03:43:29 +0900

    Add files via upload
    【add】：javascript、jQueryによるページ遷移時及びスクロール時のフェードインアニメーションの追加

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// SP用メニューバー
 {
   const open = document.getElementById('open');
   const overlay = document.querySelector('.overlay');
@@ -16,3 +16,18 @@
   });
 }
 
+// フェードイン
+{
+  let fadeInTarget = document.querySelectorAll('.fade-in ,.fade-in2');
+  window.addEventListener('scroll', () => {
+    for (let i = 0; i < fadeInTarget.length; i++){
+      const rect = fadeInTarget[i].getBoundingClientRect().top;
+      const scroll = window.pageYOffset || document.documentElement.scrollTop;
+      const offset = rect + scroll;
+      const windowHeight = window.innerHeight; // 現在のブラウザの高さ
+      if (scroll > offset - windowHeight + 120) {
+        fadeInTarget[i].classList.add('scroll-in');
+      }
+    }
+  });
+}
